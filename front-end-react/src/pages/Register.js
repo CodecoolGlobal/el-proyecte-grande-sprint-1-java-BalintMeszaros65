4505161465b92ba1b8ac90f9b73a8bd8a3d4a96b
megaseves <megaseves@gmail.com>
2022-11-17 12:24:26 +0100

    Add: base structure of registration page

--- front-end-react/src/pages/Register.js
+++ front-end-react/src/pages/Register.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {Grid, Paper, Typography, TextField, Button} from '@mui/material';
+
 
 export function Register(){
     return (
-        <div>Register</div>
+
+
+        <Grid>
+            <Paper>
+
+                <Grid align={'center'}>
+                    <h2>Sign Up</h2>
+                    <Typography variant={'caption'}>Fill this form for create an account!</Typography>
+                </Grid>
+                <form>
+                    <TextField fullWidth label={'Git Profile'} />
+                    <TextField fullWidth label={'Journey Profile'} />
+                    <TextField fullWidth label={'Username'} />
+                    <TextField fullWidth label={'E-mail'} />
+                    <TextField fullWidth label={'Password'} />
+                    <TextField fullWidth label={'Password re'} />
+                    <Button type={'submit'} variant={'contained'} color={'primary'}>Sign up</Button>
+                </form>
+            </Paper>
+        </Grid>
+
     )
 }
