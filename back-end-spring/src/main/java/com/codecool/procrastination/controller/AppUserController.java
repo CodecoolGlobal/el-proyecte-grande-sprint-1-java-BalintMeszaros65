a1870d580a74c1447dda3ef5fab0faa45879d1de
Balint Meszaros <meszaros.balint65@gmail.com>
2022-11-29 14:59:58 +0100

    Add: modelMapper

--- back-end-spring/src/main/java/com/codecool/procrastination/controller/AppUserController.java
+++ back-end-spring/src/main/java/com/codecool/procrastination/controller/AppUserController.java
@@ -2,15 +2,24 @@ package com.codecool.procrastination.controller;
 
 import com.codecool.procrastination.service.AppUserService;
 import com.codecool.procrastination.model.AppUser;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppUserController {
 
-    @Autowired
-    private AppUserService appUserService;
+    private final AppUserService appUserService;
+    private final ModelMapper modelMapper;
 
+    @Autowired
+    public AppUserController(AppUserService appUserService, ModelMapper modelMapper) {
+        this.appUserService = appUserService;
+        this.modelMapper = modelMapper;
+    }
+
+
+    // TODO return response error if the appUser misses attributes
     @PostMapping("/api/user/registration")
     public void registerUser(@RequestBody AppUser appUser) {
         appUserService.saveUser(appUser);
