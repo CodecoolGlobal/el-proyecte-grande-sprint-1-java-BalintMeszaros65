fd1a670a010d84ca98e9760c09016a0660c8a185
Qephetzial <prekopadominik@gmail.com>
2022-11-30 11:06:47 +0100

    Add: ProjectService class implemented(get/save project, addNewMember, changProjectStatus)

---
+++ back-end-spring/src/main/java/com/codecool/procrastination/service/ProjectService.java
@@ -0,0 +1,40 @@
+package com.codecool.procrastination.service;
+
+import com.codecool.procrastination.model.AppUser;
+import com.codecool.procrastination.model.Project;
+import com.codecool.procrastination.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public void saveProject (Project project) {projectRepository.save(project);}
+
+    public Project getProjectById (UUID id) {
+        Optional<Project> project = projectRepository.findById(id);
+        return project.orElse(null);
+    }
+
+    public void addNewMember (UUID id, AppUser appUser) {
+        Project project = getProjectById(id);
+        project.addNewMember(appUser);
+        projectRepository.save(project);
+    }
+
+    public void changeProjectStatus(UUID id) {
+        Project project = getProjectById(id);
+        project.changeStatus();
+        projectRepository.save(project);
+    }
+}
