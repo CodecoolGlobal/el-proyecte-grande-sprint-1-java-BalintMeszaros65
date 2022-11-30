b74d0bfd33dfa6b60eae6aeeb639ccd8300a8283
Balint Meszaros <meszaros.balint65@gmail.com>
2022-11-30 13:54:03 +0100

    Add: sql init file for AppUser

---
+++ back-end-spring/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO app_user (id, email, git_profile, journey_profile, password, user_name)
+VALUES ('68229fa9-e9bc-4c8a-94c4-25aca83a30a9', 'testemail@gmail.com', 'testgitprofile', 'testjourneyprofile',
+        'testpassword', 'testuser');
+INSERT INTO app_user (id, email, git_profile, journey_profile, password, user_name)
+VALUES ('68229fa9-e9bc-4c8a-94c4-25aca83a30a1', 'testemail2@gmail.com', 'testgitprofile2', 'testjourneyprofile2',
+        'testpassword2', 'testuser2');
