4269f068dd9dc37513ed313214fb051644361480
M9Bene <bene9marci@gmail.com>
2022-12-01 10:00:00 +0100

    Add: basic react elements

--- front-end-react/src/pages/Login.js
+++ front-end-react/src/pages/Login.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Login.css';
 
 export function Login(){
+
+    const [userNameFormValue, setUserNameFormValue] = useState('');
+    const [passwordFormValue, setPasswordFormValue] = useState('');
+
+    function isFormFilled(){
+        // todo implement
+        console.log("check if forms filled")
+    }
+
+    function fetchForLogin(){
+        // todo implement
+        console.log("trying to fetch login")
+    }
+
+
     return (
-        <div>Login</div>
+        <div className={'login-form-container'}>
+            <form>
+                <h2>Login</h2>
+
+                <p>Username</p>
+                <input type="text"  name={'username'} placeholder={'Enter your Username'}
+                onChange={(value) => setUserNameFormValue(value.target.value)}/>
+
+                <p>Password</p>
+                <input type="text"  name={'password'} placeholder={'Enter your Password'}
+                       onChange={(value) => setPasswordFormValue(value.target.value)}/>
+
+                {(isFormFilled()) && <button type={'submit'} onClick={async (e) => {
+                    e.preventDefault()
+                    await fetchForLogin()
+                }}>
+                    Sign up
+                </button>}
+
+            </form>
+        </div>
     )
 }
