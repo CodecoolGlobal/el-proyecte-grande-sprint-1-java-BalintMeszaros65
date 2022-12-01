0119161deef5ecc08b0754dd16f8a481c6615955
Qephetzial <prekopadominik@gmail.com>
2022-12-01 11:38:32 +0100

    Add: Project class @Notnull annotations

--- back-end-spring/src/main/java/com/codecool/procrastination/model/Project.java
+++ back-end-spring/src/main/java/com/codecool/procrastination/model/Project.java
@@ -6,28 +6,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 @Entity
 public class Project {
-    // TODO NotNull annotations
     // TODO gitRepo unique
 
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
+    @NotNull
     private String projectName;
+    @NotNull
     private String teamName;
+    @NotNull
     private String gitRepo;
     private boolean finished;
-
     @ManyToMany
     private Set<AppUser> members;
 
     public Project() {
         this.finished = false;
+        this.members = new HashSet<>();
     }
 
     public UUID getId() {
