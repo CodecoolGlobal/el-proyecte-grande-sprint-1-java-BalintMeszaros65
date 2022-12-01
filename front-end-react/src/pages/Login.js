bb6f804efc1f29866c69368210f8878ed84d2239
M9Bene <bene9marci@gmail.com>
2022-12-01 15:34:16 +0100

    Add:  problems to solve

--- front-end-react/src/pages/Login.js
+++ front-end-react/src/pages/Login.js
@@ -1,48 +1,72 @@
 import React, {useState} from 'react';
 import './Login.css';
+import {useNavigate} from "react-router-dom";
 
-export function Login(){
+export function Login(props) {
+    const navigate = useNavigate();
 
-    const [emailFormValue, setEmailFormValue] = useState('');
-    const [passwordFormValue, setPasswordFormValue] = useState('');
+    const [formData, setFormData] = useState(
+        {
+            'email': '',
+            'password': ''
+        });
+    const [badLogin, setBadLogin] = useState(false);
+    const [loggedInUserID, setLoggedInUserId] = useState('');
 
-    function isFormFilled(){
-        return emailFormValue.length > 2 && passwordFormValue.length > 2;
+    function isFormFilled() {
+        return formData['email'].length > 2
+                && formData['password'].length > 2
+                && validateEmail();
     }
 
-    async function fetchForLogin(){
-
-        let formData = {
-            'email': emailFormValue,
-            'password': passwordFormValue,
-        }
-
+    async function fetchForLogin() {
+        setBadLogin(false);
         const requestOptions = {
-            method: 'GET',
+            method: 'POST',
             mode: 'cors',
-            headers: { 'Content-Type': 'application/json',},
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(formData)
         };
-
-        //      ASYNC FETCH
-        const response = await fetch('http://localhost:8080/api/user/login',
-            requestOptions);
+           //   ASYNC FETCH
+        // try {
+            const data = await (await fetch('http://localhost:8080/api/user/login',
+                requestOptions)).json();
+            console.log(data)
+            // setLoggedInUserId(data);
+            // console.log(loggedInUserID);
+            navigate('/');
+        // } catch (e) {
+          //  rejectLogin()
+        // }
 
     }
 
+    function rejectLogin() {
+        formData['email'] = '';
+        formData['password'] = '';
+        setBadLogin(true);
+    }
+
+    function validateEmail() {
+        return (/\S+@\S+\.\S+/.test(formData['email']));
+    }
 
     return (
         <div className={'login-form-container'}>
             <form>
                 <h2>Login</h2>
-
+                {(badLogin) && <p className={'error'}><small>E-Mail or Password is invalid!</small></p>}
                 <p>E-mail</p>
-                <input type="text"  name={'email'} placeholder={'Enter your E-mail'}
-                onChange={(value) => setEmailFormValue(value.target.value)}/>
+                <input value={formData['email']} type="text" name={'email'} placeholder={'Enter your E-mail'}
+                       onChange={(value) => {
+                           setFormData(prevFormData => ({...prevFormData, 'email': value.target.value}))
+                       }}/>
 
                 <p>Password</p>
-                <input type="text"  name={'password'} placeholder={'Enter your Password'}
-                       onChange={(value) => setPasswordFormValue(value.target.value)}/>
+                <input value={formData['password']} type="password" name={'password'} placeholder={'Enter your Password'}
+                       onChange={(value) => {
+                           setFormData(prevFormData => ({...prevFormData, 'password': value.target.value}))
+                       }}/>
 
                 {(isFormFilled()) && <button type={'submit'} onClick={async (e) => {
                     e.preventDefault()
@@ -50,7 +74,6 @@ export function Login(){
                 }}>
                     Login
                 </button>}
-
             </form>
         </div>
     )
