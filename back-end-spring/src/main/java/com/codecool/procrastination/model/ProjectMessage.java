b9e4f1ace600a07a61517ba6049a3bae815eb325
Balint Meszaros <meszaros.balint65@gmail.com>
2022-12-01 10:56:09 +0100

    Add: ProjectMessage

---
+++ back-end-spring/src/main/java/com/codecool/procrastination/model/ProjectMessage.java
@@ -0,0 +1,54 @@
+package com.codecool.procrastination.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+public class ProjectMessage {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    UUID id;
+    @NotNull
+    @ManyToOne
+    Project project;
+    @NotNull
+    @ManyToOne
+    AppUser appUser;
+    @NotNull
+    String message;
+    @CreationTimestamp
+    Timestamp timestamp;
+
+    public ProjectMessage() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProjectMessage that = (ProjectMessage) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return timestamp.toString()  +
+                " - " +
+                appUser.getUserName() +
+                ": " +
+                message;
+    }
+}
