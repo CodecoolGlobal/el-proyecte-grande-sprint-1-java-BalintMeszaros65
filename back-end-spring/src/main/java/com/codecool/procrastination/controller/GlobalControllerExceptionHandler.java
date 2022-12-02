4c7696d5d7782480281120251916b234eefccf37
Bence Fabian <bence.fabian@codecool.com>
2022-12-02 12:13:24 +0100

    make exception handlers return a message

--- back-end-spring/src/main/java/com/codecool/procrastination/controller/GlobalControllerExceptionHandler.java
+++ back-end-spring/src/main/java/com/codecool/procrastination/controller/GlobalControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.codecool.procrastination.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.rmi.AlreadyBoundException;
@@ -12,15 +13,24 @@ import java.util.NoSuchElementException;
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoSuchElementException.class)
-    public void handleNotFound() {}
+    @ResponseBody
+    public String handleNotFound(NoSuchElementException e) {
+        return e.getMessage();
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({SQLException.class, NullPointerException.class, AlreadyBoundException.class})
-    public void handleBadSQLRequest() {}
+    @ResponseBody
+    public String handleBadSQLRequest(Throwable e) {
+        return e.getMessage();
+    }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(IllegalAccessException.class)
-    public void handleUnauthorizedRequest() {}
+    @ResponseBody
+    public String handleUnauthorizedRequest(IllegalAccessException e) {
+        return e.getMessage();
+    }
 }
