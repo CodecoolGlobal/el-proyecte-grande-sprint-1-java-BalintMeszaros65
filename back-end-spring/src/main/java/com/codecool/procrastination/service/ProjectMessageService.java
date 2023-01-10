e98dda00b0ee2b0f7a03a33a8665c6df65163da5
Balint Meszaros <meszaros.balint65@gmail.com>
2023-01-10 13:58:34 +0100

    Refactor: DB connection to match authentication

--- back-end-spring/src/main/java/com/codecool/procrastination/service/ProjectMessageService.java
+++ back-end-spring/src/main/java/com/codecool/procrastination/service/ProjectMessageService.java
@@ -5,7 +5,11 @@ import com.codecool.procrastination.model.AppUser;
 import com.codecool.procrastination.model.Project;
 import com.codecool.procrastination.model.ProjectMessage;
 import com.codecool.procrastination.repositories.ProjectMessageRepository;
+import com.codecool.procrastination.repositories.ProjectRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,27 +19,38 @@ import java.util.stream.Collectors;
 @Service
 public class ProjectMessageService {
 
+    Logger log = LoggerFactory.getLogger(ProjectMessageService.class);
+
     private final ProjectMessageRepository projectMessageRepository;
     private final ProjectService projectService;
     private final AppUserService appUserService;
+    private final ProjectRepository projectRepository;
 
     @Autowired
-    public ProjectMessageService(ProjectMessageRepository projectMessageRepository, ProjectService projectService, AppUserService appUserService) {
+    public ProjectMessageService(ProjectMessageRepository projectMessageRepository, ProjectService projectService, AppUserService appUserService,
+                                 ProjectRepository projectRepository) {
         this.projectMessageRepository = projectMessageRepository;
         this.projectService = projectService;
         this.appUserService = appUserService;
+        this.projectRepository = projectRepository;
     }
 
     public List<String> getAllMessagesByProjectIdOrderedByTimestamp(UUID projectId) {
-        List<ProjectMessage> projectMessages = projectMessageRepository.findAllByProjectIdOrderByTimestamp(projectId);
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        AppUser appUser = appUserService.getUserByEmail(email);
+        // TODO authenticate from context if the user is a member of the project
+        // TODO FIX jpa not working
+        List<ProjectMessage> projectMessages = projectMessageRepository.getAllByProjectIdOrderByTimestamp(projectId);
         return projectMessages.stream()
                 .map(ProjectMessage::toString)
                 .collect(Collectors.toList());
     }
 
-    public void saveProjectMessage(UUID projectId, UUID userId,  ProjectMessage projectMessage) {
+    public void saveProjectMessage(UUID projectId, ProjectMessage projectMessage) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
         Project project = projectService.getProjectById(projectId);
-        AppUser appUser = appUserService.getUserById(userId);
+        AppUser appUser = appUserService.getUserByEmail(email);
+        // TODO authenticate from context if the user is a member of the project
         if (projectMessage.getMessage() == null) {
             throw new CustomExceptions.MissingAttributeException("Missing message.\n");
         } else {
