59a5de6deabbf012bd0b00cb44f3a102004b0d1e
M9Bene <bene9marci@gmail.com>
2023-01-26 16:11:25 +0100

    Add: fetch for user details

--- front-end-react/src/pages/Profile.js
+++ front-end-react/src/pages/Profile.js
@@ -1,16 +1,55 @@
-import React from "react";
-import {Navbar} from "../components/Navbar";
+import React, {useEffect, useState} from "react";
+import {getTokenForCurrentUser} from "../components/RouteGuard";
 
 
 export function Profile() {
 
+    const [userDetails, setUserDetails] = useState([]);
+
+
+    useEffect(() => {
+        getUserDetails();
+    })
+
+    async function getUserDetails() {
+        const user_id = getTokenForCurrentUser();
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${user_id}`,
+                'Content-Type': 'application/json'
+            }
+        }
+        await fetch(`/api/user/get-details`, requestOptions)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                    setUserDetails(data)
+                }
+            )
+    }
 
 
     return (
         <div className={'profile-container'}>
-            <h1 >Profile Page</h1>
+            <h1>Profile Page</h1>
             <div className={'user-datas'}>
-                <p>datas about project (git,journey,username,email)</p>
+                <p>
+                    {userDetails.userName}
+                </p>
+                <p>
+                    {userDetails.email}
+                </p>
+                <p>
+                    {userDetails.gitProfile}
+                </p>
+                <p>
+                    {userDetails.journeyProfile}
+                </p>
+
             </div>
             <div className={'user-teams'}>
                 <p>teams of user</p>
